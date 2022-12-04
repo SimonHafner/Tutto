320b429d69cca289512c4d2d5c907be8ade0e605
SimonHafner <simon.gabriel.hafner@gmail.com>
2022-12-04 17:17:50 +0100

    Added some comments to "TestDice"

--- Test/DiceTest.java
+++ Test/DiceTest.java
@@ -7,11 +7,12 @@ class DiceTest {
     @Test
     void throwDice() {
         Dice dice = new Dice();
+        // By creating a new dice, the die is set to 0 (i.e. undefined) by default.
         int dieOne = dice.getDie();
         dice.throwDice();
+        // The die should change to a number between 1 and 6.
         int dieTwo = dice.getDie();
         assertNotEquals(dieOne, dieTwo);
-        // However, keep in mind that it's either possible that the same die can occur two times in a row.
     }
 
     @Test
